6a8a3e0fe7c4269601bf9026bca5cb6e450bd1a9
Darek Mazur <dev@darekmazur.it>
2024-11-07 02:59:30 +0100

    Install dotenv

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
 const index = express();
 const port = process.env.PORT || 3000;
+
+dotenv.config()
 
 index.get('/', (req, res) => {
 	res.send('Hello World!');
