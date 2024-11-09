35168ecd8d63fdcd1cc6a6ca84d59558b433a6f3
Darek Mazur <dev@darekmazur.it>
2024-11-09 12:46:17 +0100

    Connect to database

--- apis/peaks.ts
+++ apis/peaks.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import getFromDatabase from "../lib/getFromDatabase";
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-	res.send('peaks API patch');
-});
+router.get('/', async (_req, res) => {
+	await getFromDatabase('peaks', res)
+})
 
 export default router;
--- apis/roles.ts
+++ apis/roles.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import getFromDatabase from "../lib/getFromDatabase";
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-	res.send('roles API patch');
-});
+router.get('/', async (_req, res) => {
+	await getFromDatabase('roles', res)
+})
 
 export default router;
