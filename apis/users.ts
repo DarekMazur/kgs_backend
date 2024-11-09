48bb480365da027de54c50aa5396d6cc674a3ebd
Darek Mazur <dev@darekmazur.it>
2024-11-09 15:39:42 +0100

    Create schema for adding new user

--- apis/users.ts
+++ apis/users.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 import getFromDatabase from "../lib/getFromDatabase";
 const router = express.Router();
+
+router.use(express.json());
 
 router.get('/', async (_req, res) => {
 	await getFromDatabase('users', res)
@@ -10,6 +13,28 @@ router.get("/:itemId", async (req, res) => {
 	const itemId = req.params.itemId;
 
 	await getFromDatabase('users', res, itemId)
+});
+
+router.post("/", (req, res) => {
+	const newUser = {
+		id: uuidv4(),
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		avatar: req.body.avatar,
+		description: req.body.description,
+		registrationDate: req.body.registrationDate,
+		isBanned: false,
+		suspensionTimeout: null,
+		totalSuspensions: 0,
+		isConfirmed: false,
+		messages: [],
+		role_id: req.body.role.id,
+	}
+
+	res.status(200).send(newUser);
 });
 
 export default router;
