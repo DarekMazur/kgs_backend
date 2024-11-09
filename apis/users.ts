9ef3cbd1547c5e260c296359d176015ee12b07ac
Darek Mazur <dev@darekmazur.it>
2024-11-09 12:34:06 +0100

    Config database client

--- apis/users.ts
+++ apis/users.ts
@@ -1,8 +1,29 @@
-import express from 'express';
+import express, {Response} from "express";
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-	res.send('users API patch');
-});
+import {pool} from "../client";
+
+const getFromDatabase = async (table: string, res: Response) => {
+	const client = await pool.connect()
+
+	if (client) {
+		console.log('Connected to database');
+		const result = await client.query(`SELECT * FROM ${table}`);
+		if (result !== undefined) {
+			res.status(200).send(result.rows);
+			console.log('Results sent')
+			client.release()
+			console.log('Client released');
+		} else {
+			res.status(400).send('No such results');
+		}
+	} else {
+		res.status(500).send('Connection failed');
+	}
+}
+
+router.get('/', async (_req, res) => {
+	await getFromDatabase('users', res)
+})
 
 export default router;
