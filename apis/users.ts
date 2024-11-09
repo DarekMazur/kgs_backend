2bf0cdad664d4bcc06b662c3512d1b4644335b24
Darek Mazur <dev@darekmazur.it>
2024-11-09 12:36:09 +0100

    Move handler to lib

--- apis/users.ts
+++ apis/users.ts
@@ -1,26 +1,6 @@
-import express, {Response} from "express";
+import express from "express";
+import getFromDatabase from "../lib/getFromDatabase";
 const router = express.Router();
-
-import {pool} from "../client";
-
-const getFromDatabase = async (table: string, res: Response) => {
-	const client = await pool.connect()
-
-	if (client) {
-		console.log('Connected to database');
-		const result = await client.query(`SELECT * FROM ${table}`);
-		if (result !== undefined) {
-			res.status(200).send(result.rows);
-			console.log('Results sent')
-			client.release()
-			console.log('Client released');
-		} else {
-			res.status(400).send('No such results');
-		}
-	} else {
-		res.status(500).send('Connection failed');
-	}
-}
 
 router.get('/', async (_req, res) => {
 	await getFromDatabase('users', res)
---
+++ lib/getFromDatabase.ts
@@ -0,0 +1,23 @@
+import {Response} from "express";
+import {pool} from "../client";
+
+const getFromDatabase = async (table: string, res: Response) => {
+	const client = await pool.connect()
+
+	if (client) {
+		console.log('Connected to database');
+		const result = await client.query(`SELECT * FROM ${table}`);
+		if (result !== undefined) {
+			res.status(200).send(result.rows);
+			console.log('Results sent')
+			client.release()
+			console.log('Client released');
+		} else {
+			res.status(400).send('No such results');
+		}
+	} else {
+		res.status(500).send('Connection failed');
+	}
+}
+
+export default getFromDatabase;
