baa0e8bfb0361ff60d617c1247cb2073477fc4ad
Darek Mazur <dev@darekmazur.it>
2024-11-09 13:05:41 +0100

    Get single user api

--- apis/users.ts
+++ apis/users.ts
@@ -6,4 +6,10 @@ router.get('/', async (_req, res) => {
 	await getFromDatabase('users', res)
 })
 
+router.get("/:itemId", async (req, res) => {
+	const itemId = req.params.itemId;
+
+	await getFromDatabase('users', res, itemId)
+});
+
 export default router;
--- lib/getFromDatabase.ts
+++ lib/getFromDatabase.ts
@@ -1,14 +1,18 @@
 import {Response} from "express";
 import {pool} from "../client";
 
-const getFromDatabase = async (table: string, res: Response) => {
+const getFromDatabase = async (table: string, res: Response, id?: string | number) => {
 	const client = await pool.connect()
 
 	if (client) {
 		console.log('Connected to database');
 		const result = await client.query(`SELECT * FROM ${table}`);
 		if (result !== undefined) {
-			res.status(200).send(result.rows);
+			let rows = [...result.rows];
+			if (id) {
+				rows = rows.filter((row) => row.id === id);
+			}
+			res.status(200).send(rows);
 			console.log('Results sent')
 			client.release()
 			console.log('Client released');
