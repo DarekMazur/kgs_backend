c5f5a5071e062290c2d5dfa341e80d5af54c7423
Darek Mazur <dev@darekmazur.it>
2024-11-11 00:14:06 +0100

    Style activation messagge

--- lib/sendMail.ts
+++ lib/sendMail.ts
@@ -19,7 +19,19 @@ const sendMail = ({email, username, token}) => {
 		from: 'Korona Gór Świętokrzyskich <kontakt@nerdistry.pl>',
 		to: email,
 		subject: `Korona Gór Świętokrzyskich - utworzono konto Użytkownika ${username}`,
-		text: `${process.env.API_HOST}/api/users/activation/${token}`,
+		text: `Konto Użytkownika ${username} zostało utworzone!
+Konto aktywujesz pod linkiem: ${process.env.API_HOST}/api/users/activation/${token}
+Link aktywacyjny jest ważny przez 24 godziny.`,
+		html: `<body style="width: 100%; height: 100%; background-color: #272724; color: #eef7eb; padding: 2rem">
+		<div style="background: url(https://res.cloudinary.com/ddyqnp7pp/image/upload/v1731279976/logoFullW_lylrnm.png) center/contain no-repeat; margin: 2rem; width: 100vw; height: 200px;"></div>
+		<h1 style="font-weight: bold; margin-bottom: 2rem">Konto Użytkownika ${username} zostało utworzone!</h1>
+		<p style="overflow-wrap: break-word">Konto aktywujesz pod linkiem: <a href="${process.env.API_HOST}/api/users/activation/${token}">${process.env.API_HOST}/api/users/activation/${token}</a></p>
+		<p>Link aktywacyjny jest ważny przez 24 godziny.</p>
+		<div style="margin-top: 3rem">
+			<p>Pozdrawiamy</p>
+			<p style="font-weight: bold">Zespół Korony Gór Świętokrzyskich</p>
+		</div>
+		</body>`
 	};
 
 	transporter.sendMail(mailOptions, (err) => {
