f4641f1c21323c702cf2d2da7fb2da90c0c70084
Darek Mazur <dev@darekmazur.it>
2024-11-11 00:33:25 +0100

    Create new route for activation

--- apis/users.ts
+++ apis/users.ts
@@ -141,50 +141,6 @@ router.post("/", async (req, res) => {
 	}
 });
 
-router.get("/activation/:auth", async (req, res) => {
-	const token = req.params.auth
-
-	let decoded: string | JwtPayload
-
-	try {
-		decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
-	} catch (error) {
-		res.status(403).send(`Authentication failed: ${error.message}`).end();
-		return
-	}
-
-	if (!decoded) {
-		res.status(403).send('Authentication failed').end();
-	}
-
-	const client = await pool.connect()
-
-	if (!client) {
-		res.status(500).send('Connection failed').end();
-	}
-
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-expect-error
-	const user = await client.query(`SELECT * FROM users WHERE id=($1)`, [decoded.id])
-
-	if (!user || user.rows.length === 0) {
-		res.status(403).send('Authentication failed').end();
-	}
-
-	if (user.rows[0].is_confirmed) {
-		res.status(403).json('User already activated').end();
-	} else {
-		try {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-expect-error
-			await client.query(`UPDATE users SET is_confirmed=true WHERE id=($1)`, [decoded.id])
-			res.status(200).send('User activated').end();
-		} catch (error) {
-			res.status(500).send(error.message);
-		}
-	}
-})
-
 router.put("/:itemId", async (req, res) => {
 	const itemId = req.params.itemId;
 
--- lib/sendMail.ts
+++ lib/sendMail.ts
@@ -20,12 +20,12 @@ const sendMail = ({email, username, token}) => {
 		to: email,
 		subject: `Korona Gór Świętokrzyskich - utworzono konto Użytkownika ${username}`,
 		text: `Konto Użytkownika ${username} zostało utworzone!
-Konto aktywujesz pod linkiem: ${process.env.API_HOST}/api/users/activation/${token}
+Konto aktywujesz pod linkiem: ${process.env.API_HOST}/confirm/${token}
 Link aktywacyjny jest ważny przez 24 godziny.`,
 		html: `<body style="width: 100%; height: 100%; background-color: #272724; color: #eef7eb; padding: 2rem">
 		<div style="background: url(https://res.cloudinary.com/ddyqnp7pp/image/upload/v1731279976/logoFullW_lylrnm.png) center/contain no-repeat; margin: 2rem; width: 100vw; height: 200px;"></div>
 		<h1 style="font-weight: bold; margin-bottom: 2rem">Konto Użytkownika <span style="color: #d99e1a">${username}</span> zostało utworzone!</h1>
-		<p style="overflow-wrap: break-word">Konto aktywujesz pod linkiem: <a href="${process.env.API_HOST}/api/users/activation/${token}">${process.env.API_HOST}/api/users/activation/${token}</a></p>
+		<p style="overflow-wrap: break-word">Konto aktywujesz pod linkiem: <a href="${process.env.API_HOST}/confirm/${token}">${process.env.API_HOST}/confirm/${token}</a></p>
 		<p>Link aktywacyjny jest ważny przez 24 godziny.</p>
 		<div style="margin-top: 3rem">
 			<p>Pozdrawiamy</p>
