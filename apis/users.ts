ab879b8cbf4181cd11ca2120eebcd87c6b0c0c86
Darek Mazur <dev@darekmazur.it>
2024-11-10 03:42:01 +0100

    Handle update user

--- apis/users.ts
+++ apis/users.ts
@@ -55,6 +55,51 @@ router.post("/", async (req, res) => {
 	}
 });
 
+router.put("/:itemId", async (req, res) => {
+	const itemId = req.params.itemId;
+
+	const client = await pool.connect()
+
+	if (client) {
+		const responseUser = await client.query(`SELECT * FROM users WHERE id=($1)::uuid`, [itemId]);
+		const roles = await client.query('SELECT * FROM roles');
+
+		const user = responseUser.rows[0];
+
+		const updatedUser = {
+			username: req.body.username ?? user.username,
+			email: user.email,
+			password: req.body.password ?? user.password,
+			firstname: req.body.firstName ?? user.firstname,
+			lastname: req.body.lastName ?? user.lastname,
+			avatar: req.body.avatar ?? user.avatar,
+			description: req.body.description ?? user.description,
+			is_banned: req.body.isBanned === undefined ? user.is_banned : req.body.is_banned,
+			suspension_timeout: req.body.suspensionTimeout ?? user.suspension_timeout,
+			total_suspensions: req.body.totalSuspensions ?? user.total_suspensions,
+			is_confirmed: req.body.isConfirmed === undefined ? user.is_confirmed : req.body.isConfirmed,
+			messages: req.body.messages ?? user.messages ?? [],
+			role_id: roles.rows.filter(role => role.id === (req.body.role?.id ?? user.role_id))[0].id,
+		}
+
+		try {
+			if (updatedUser.suspension_timeout) {
+				await client.query(`UPDATE users SET suspension_timeout='${updatedUser.suspension_timeout}' WHERE id=($1)::uuid`, [user.id])
+			}
+
+			if (updatedUser.messages && updatedUser.messages.length > 0) {
+				await client.query(`UPDATE users SET messages='${updatedUser.messages}' WHERE id=($1)::uuid`, [user.id])
+			}
+			await client.query(`UPDATE users SET username='${updatedUser.username}', email='${updatedUser.email}', password='${updatedUser.password}', firstname='${updatedUser.firstname}', lastname='${updatedUser.lastname}', avatar='${updatedUser.avatar}', description='${updatedUser.description}', is_banned='${updatedUser.is_banned}', total_suspensions='${updatedUser.total_suspensions}', is_confirmed='${updatedUser.is_confirmed}', role_id='${updatedUser.role_id}' WHERE id=($1)::uuid`, [user.id]);
+			res.status(200).send(updatedUser).end();
+		} catch (error) {
+			res.status(500).send(error.message);
+		}
+	} else {
+		res.status(500).send('Connection failed');
+	}
+})
+
 router.delete("/:itemId", async (req, res) => {
 	const itemId = req.params.itemId;
 
--- lib/deleteFromDatabase.ts
+++ lib/deleteFromDatabase.ts
@@ -5,7 +5,6 @@ const deleteFromDatabase = async (table: string, res: Response, id: string) => {
 	const client = await pool.connect()
 
 	if (client) {
-		console.log(id)
 		try {
 			await client.query(`DELETE FROM ${table} WHERE id=($1)::uuid`, [id]);
 			res.status(200).send('Deleted')
