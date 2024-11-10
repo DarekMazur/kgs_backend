c2ad6389e4dac6356e816855f11e54d4bd726481
Darek Mazur <dev@darekmazur.it>
2024-11-10 21:02:13 +0100

    Create activation route

--- apis/users.ts
+++ apis/users.ts
@@ -127,6 +127,32 @@ router.post("/", async (req, res) => {
 	}
 });
 
+router.put("/activation/:itemId", async (req, res) => {
+	const itemId = req.params.itemId
+	const client = await pool.connect()
+
+	if (!client) {
+		res.status(500).send('Connection failed').end();
+	}
+
+	const user = await client.query(`SELECT * FROM users WHERE id = ($1)`, [itemId])
+
+	if (!user || user.rows.length === 0) {
+		res.status(403).send('Authentication failed').end();
+	}
+
+	if (user.rows[0].is_confirmed) {
+		res.status(403).json('User already activated').end();
+	} else {
+		try {
+			await client.query(`UPDATE users SET is_confirmed=true WHERE id=($1)`, [itemId])
+			res.status(200).send('User activated').end();
+		} catch (error) {
+			res.status(500).send(error.message);
+		}
+	}
+})
+
 router.put("/:itemId", async (req, res) => {
 	const itemId = req.params.itemId;
 
