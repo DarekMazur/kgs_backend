842cb546c4fb40d661debf1f88aa3ea539fa7b32
Darek Mazur <dev@darekmazur.it>
2024-11-11 20:49:56 +0100

    Style input

--- apis/reset-password.ts
+++ apis/reset-password.ts
@@ -9,7 +9,7 @@ import sendMail from "../lib/sendMail";
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 
-const recoveryView = (id: string) => {
+const recoveryView = (id: string, username: string) => {
 	return (
 		`<!doctype html>
 		<html lang="pl">
@@ -18,13 +18,51 @@ const recoveryView = (id: string) => {
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 				<title>Korona Gór Śwętokrzyskich | Reset</title>
 		</head>
-		<body style="width: 100%; height: 100%; background-color: #272724; color: #eef7eb; padding: 2rem">
+		<body style="max-width: 100vw; background-color: #272724; color: #eef7eb; padding: 2rem">
 			<div style="background: url(https://res.cloudinary.com/ddyqnp7pp/image/upload/v1731279976/logoFullW_lylrnm.png) center/contain no-repeat; margin: 2rem; width: 100vw; height: 200px;"></div>
-			<h3 style="font-weight: bold; margin-bottom: 2rem; text-align: center">Podaj nowe hasło</h3>
-			<form method="POST" action="/reset-password">
-				<input type="password" name="password" id="password" placeholder="Nowe hasło" required />
+			<h2 style="font-weight: bold; margin: 3rem 0 2rem; text-align: center">Zresetuj hasło Użytkownika <span style="color: #d99e1a">${username}</span></h2>
+			<form method="POST" action="/reset-password" style="display: flex; justify-content: center; align-items: center">
+			<fieldset style="padding: 4rem 3rem; max-width: 35rem; display: flex; justify-content: center; align-items: center">
+			<legend>Ustaw nowe hasło</legend>
+				<input type="password" name="password" id="password" placeholder="Nowe hasło" required style="
+					border-radius: 5px;
+					background: rgb(249, 250, 250);
+					border: 1px solid rgb(181, 189, 196);
+					font-size: 16px;
+					height: 30px;
+					line-height: 24px;
+					padding: 7px 8px;
+					color: rgb(8, 9, 10);
+					box-shadow: none;
+					:focus{
+						background-color: #fff;
+						border-color: #3b49df;
+						box-shadow: 1px 1px 0 #3b49df;
+					}" 
+				/>
 				<input type="hidden" name="id" value="${id}" />
-				<input type="submit" value="Zatwierdź">
+				<input type="submit" value="Zatwierdź" style="
+					display: inline-block;
+					outline: none;
+					cursor: pointer;
+					font-size: 14px;
+					line-height: 1;
+					border-radius: 500px;
+					border: 1px solid transparent;
+					letter-spacing: 2px;
+					min-width: 160px;
+					text-transform: uppercase;
+					white-space: normal;
+					font-weight: 700;
+					text-align: center;
+					padding: 16px 14px 18px;
+					margin-left: 2rem;
+					color: #c1c0c0;
+					box-shadow: inset 0 0 0 2px #c1c0c0;
+					background-color: transparent;
+					height: 48px;
+				">
+				</fieldset>
 			</form>
 		</body>`
 	)
@@ -109,13 +147,12 @@ Link jest ważny przez 24 godziny.`
 	sendMail(options)
 
 	res.status(200).send('Wysłano email')
-
 })
 
 router.get('/:token', async (req, res) => {
 	if (req.params.token) {
 		await statusAction(req.params.token, res).then((response) => {
-			res.send(recoveryView(response.id))
+			res.send(recoveryView(response.id, response.username))
 		})
 	} else {
 		res.status(404).send('Invalid or expired token');
