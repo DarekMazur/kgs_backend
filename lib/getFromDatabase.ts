2d21d97bb6164192ead9191559749ae032378f33
Darek Mazur <dev@darekmazur.it>
2024-11-11 22:29:47 +0100

    Prepare posts data template

--- lib/getFromDatabase.ts
+++ lib/getFromDatabase.ts
@@ -35,6 +35,29 @@ const getFromDatabase = async (table: string, res: Response, id?: string | numbe
 					role: roles.rows.filter(role => role.id === user.role_id)[0],
 				}))
 			}
+
+			if ( table === 'posts' ) {
+				const users = await client.query('SELECT id, username, firstname, avatar, suspension_timeout, is_banned, role_id FROM users');
+				const peaks = await client.query('SELECT * FROM peaks');
+
+				rows = rows.map(post => ({
+					id: post.id,
+					createdAt: new Date(post.created_at),
+					notes: post.notes,
+					photo: post.photo,
+					peak:	peaks.rows.filter(peak => peak.id === post.peak_id)[0],
+					isHidden: post.is_hidden,
+					author: users.rows.filter(user => user.id === post.author_id).map(author => ({
+						id: author.id,
+						username: author.username,
+						firstName: author.firstname,
+						avatar: author.avatar,
+						isSuspended: !!author.suspension_timeout && author.suspension_timeout > Date.now(),
+						isBanned: author.is_banned,
+						role: author.role_id,
+					}))[0],
+				}))
+			}
 			res.status(200).send(rows);
 			console.log('Results sent')
 			client.release()
