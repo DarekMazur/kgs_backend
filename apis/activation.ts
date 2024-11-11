c23f46c3dab527fb3e73a00ff73ea0ae5d8b5c99
Darek Mazur <dev@darekmazur.it>
2024-11-11 02:17:39 +0100

    Style activation landing page

--- apis/activation.ts
+++ apis/activation.ts
@@ -4,6 +4,14 @@ import process from "node:process";
 import {pool} from "../client";
 const router = express.Router();
 
+const activationView = (user: string) => {
+	return (`<body style="width: 100%; height: 100%; background-color: #272724; color: #eef7eb; padding: 2rem">
+	<div style="background: url(https://res.cloudinary.com/ddyqnp7pp/image/upload/v1731279976/logoFullW_lylrnm.png) center/contain no-repeat; margin: 2rem; width: 100vw; height: 200px;"></div>
+	<h1 style="font-weight: bold; margin-bottom: 2rem; text-align: center">Konto Użytkownika <span style="color: #d99e1a">${user}</span> zostało aktywowane!</h1>
+	<p style="text-align: center">Możesz się już zalogować do aplikacji.</p>
+</body>`)
+}
+
 router.get('/:token', async (req, res) => {
 	const token = req.params.token
 
@@ -41,7 +49,7 @@ router.get('/:token', async (req, res) => {
 			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 			// @ts-expect-error
 			await client.query(`UPDATE users SET is_confirmed=true WHERE id=($1)`, [decoded.id])
-			res.status(200).send('User activated').end();
+			res.status(200).send(activationView(user.rows[0].username)).end();
 		} catch (error) {
 			res.status(500).send(error.message);
 		}
