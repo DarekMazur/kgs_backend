bb39b453287b96ffa9410554e5b23d1e5e64dfdd
Darek Mazur <dev@darekmazur.it>
2024-11-13 05:05:07 +0100

    Remove unused library

--- apis/users.ts
+++ apis/users.ts
@@ -1,4 +1,4 @@
-import express, {response} from "express";
+import express from "express";
 import { v4 as uuidv4 } from "uuid";
 import getFromDatabase from "../lib/getFromDatabase";
 import {pool} from "../client";
