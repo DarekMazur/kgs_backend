cff7804ace5b230357393ed50c21b5eb514c74c8
Darek Mazur <dev@darekmazur.it>
2024-11-13 14:29:53 +0100

    Create interfaces for API's data

---
+++ lib/types.ts
@@ -0,0 +1,93 @@
+export interface IRole {
+	id: number,
+	name: string,
+	type: string
+}
+
+export interface IMessage {
+	id: string;
+	priority: number;
+	header: string;
+	message: string;
+	sendTime: Date;
+	openedTime?: Date;
+}
+
+interface IPeak {
+	id: string,
+	name: string,
+	height: string,
+	description: string,
+	trial: string,
+	image: string,
+}
+
+interface IPost {
+	id: string,
+	notes: string,
+	photo: string,
+}
+
+interface IUser {
+	id: string,
+	username: string,
+	email: string,
+	avatar: string,
+	description?: string,
+	messages: IMessage[],
+}
+
+export interface IPublicPeak extends IPeak {
+	localizationLat: string,
+	localizationLng: string,
+}
+
+export interface IResponsePeak extends IPeak {
+	localization_lat: string,
+	localization_lng: string,
+}
+
+export interface IPublicPost extends IPost {
+	peak: IPeak;
+	isHidden: boolean;
+	createdAt: Date;
+	author: {
+		id: string,
+		username: string,
+		firstName?: string,
+		avatar: string,
+		isSuspended: boolean,
+		isBanned: boolean,
+		role: number
+	}
+}
+
+export interface IResponsePost extends IPost {
+	peak_id: string;
+	is_hidden: boolean;
+	created_at: number;
+	author_id: string;
+}
+
+export interface IPublicUser extends IUser {
+	firstName?: string,
+	lastName?: string,
+	inBanned: boolean,
+	suspensionTimeout?: Date,
+	totalSuspensions: number,
+	isConfirmed: boolean,
+	posts: IPublicPost[],
+	registrationDate: Date,
+	role: IRole,
+}
+
+export interface IResponseUser extends IUser {
+	firstname?: string,
+	lastname?: string,
+	in_banned: boolean,
+	suspension_timeout?: number,
+	total_suspensions: number,
+	is_confirmed: boolean,
+	registration_date: number,
+	role_id: number,
+}
