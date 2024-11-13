7ff95c0d0de7ca66fc43c413f925d5f57fc3b2de
Darek Mazur <dev@darekmazur.it>
2024-11-13 03:09:42 +0100

    Allow user edit only by admins

--- apis/users.ts
+++ apis/users.ts
@@ -183,43 +183,65 @@ Link aktywacyjny jest ważny przez 24 godziny.`
 router.put("/:itemId", async (req, res) => {
 	const itemId = req.params.itemId;
 
-	const client = await pool.connect()
+	const token = (req.header('Authorization' as string)?.split(' ')[1])
 
-	if (client) {
-		const responseUser = await client.query(`SELECT * FROM users WHERE id=($1)::uuid`, [itemId]);
-		const roles = await client.query('SELECT * FROM roles');
-		const salt = await bcrypt.genSalt();
+	if (authorisation(token, res, itemId, 1)) {
+		const client = await pool.connect()
 
-		const user = responseUser.rows[0];
+		if (client) {
+			const responseUser = await client.query(`SELECT *
+																							 FROM users
+																							 WHERE id = ($1)::uuid`, [itemId]);
+			const roles = await client.query('SELECT * FROM roles');
+			const salt = await bcrypt.genSalt();
 
-		const updatedUser = {
-			username: req.body.username ?? user.username,
-			email: user.email,
-			password: await hashedPassword(req.body.password + user.registration_date, salt) ?? user.password,
-			firstname: req.body.firstName ?? user.firstname,
-			lastname: req.body.lastName ?? user.lastname,
-			avatar: req.body.avatar ?? user.avatar,
-			description: req.body.description ?? user.description,
-			is_banned: req.body.isBanned === undefined ? user.is_banned : req.body.is_banned,
-			suspension_timeout: req.body.suspensionTimeout ?? user.suspension_timeout,
-			total_suspensions: req.body.totalSuspensions ?? user.total_suspensions,
-			is_confirmed: req.body.isConfirmed === undefined ? user.is_confirmed : req.body.isConfirmed,
-			messages: req.body.messages ?? user.messages ?? [],
-			role_id: roles.rows.filter(role => role.id === (req.body.role?.id ?? user.role_id))[0].id,
-		}
+			const user = responseUser.rows[0];
 
-		try {
-			if (updatedUser.suspension_timeout) {
-				await client.query(`UPDATE users SET suspension_timeout='${updatedUser.suspension_timeout}' WHERE id=($1)::uuid`, [user.id])
+			const updatedUser = {
+				username: req.body.username ?? user.username,
+				email: user.email,
+				password: await hashedPassword(req.body.password + user.registration_date, salt) ?? user.password,
+				firstname: req.body.firstName ?? user.firstname,
+				lastname: req.body.lastName ?? user.lastname,
+				avatar: req.body.avatar ?? user.avatar,
+				description: req.body.description ?? user.description,
+				is_banned: req.body.isBanned === undefined ? user.is_banned : req.body.is_banned,
+				suspension_timeout: req.body.suspensionTimeout ?? user.suspension_timeout,
+				total_suspensions: req.body.totalSuspensions ?? user.total_suspensions,
+				is_confirmed: req.body.isConfirmed === undefined ? user.is_confirmed : req.body.isConfirmed,
+				messages: req.body.messages ?? user.messages ?? [],
+				role_id: roles.rows.filter(role => role.id === (req.body.role?.id ?? user.role_id))[0].id,
 			}
 
-			if (updatedUser.messages && updatedUser.messages.length > 0) {
-				await client.query(`UPDATE users SET messages='${updatedUser.messages}' WHERE id=($1)::uuid`, [user.id])
+			try {
+				if (updatedUser.suspension_timeout) {
+					await client.query(`UPDATE users
+															SET suspension_timeout='${updatedUser.suspension_timeout}'
+															WHERE id = ($1)::uuid`, [user.id])
+				}
+
+				if (updatedUser.messages && updatedUser.messages.length > 0) {
+					await client.query(`UPDATE users
+															SET messages='${updatedUser.messages}'
+															WHERE id = ($1)::uuid`, [user.id])
+				}
+				await client.query(`UPDATE users
+														SET username='${updatedUser.username}',
+																email='${updatedUser.email}',
+																password='${updatedUser.password}',
+																firstname='${updatedUser.firstname}',
+																lastname='${updatedUser.lastname}',
+																avatar='${updatedUser.avatar}',
+																description='${updatedUser.description}',
+																is_banned='${updatedUser.is_banned}',
+																total_suspensions='${updatedUser.total_suspensions}',
+																is_confirmed='${updatedUser.is_confirmed}',
+																role_id='${updatedUser.role_id}'
+														WHERE id = ($1)::uuid`, [user.id]);
+				res.status(200).send(updatedUser).end();
+			} catch (error) {
+				res.status(500).send(error.message);
 			}
-			await client.query(`UPDATE users SET username='${updatedUser.username}', email='${updatedUser.email}', password='${updatedUser.password}', firstname='${updatedUser.firstname}', lastname='${updatedUser.lastname}', avatar='${updatedUser.avatar}', description='${updatedUser.description}', is_banned='${updatedUser.is_banned}', total_suspensions='${updatedUser.total_suspensions}', is_confirmed='${updatedUser.is_confirmed}', role_id='${updatedUser.role_id}' WHERE id=($1)::uuid`, [user.id]);
-			res.status(200).send(updatedUser).end();
-		} catch (error) {
-			res.status(500).send(error.message);
 		}
 	} else {
 		res.status(500).send('Connection failed');
@@ -231,7 +253,7 @@ router.delete("/:itemId", async (req, res) => {
 
 	const token = (req.header('Authorization' as string)?.split(' ')[1])
 
-	if (authorisation(token, res)) {
+	if (authorisation(token, res, itemId, 1)) {
 		await deleteFromDatabase('users', res, itemId)
 	}
 })
