8a83e45d9c5c2400b0c0c0825961a8cd92202b07
Darek Mazur <dev@darekmazur.it>
2024-11-17 05:50:12 +0100

    Create README documentation

--- lib/sendMail.ts
+++ lib/sendMail.ts
@@ -6,12 +6,12 @@ const sendMail = ({email, text, html, subject}) => {
 	}
 
 	const transporter = nodemailer.createTransport({
-		host: process.env.SMPT_HOST,
-		port: process.env.SMPT_PORT,
-		service: process.env.SMPT_SERVICE,
+		host: process.env.SMTP_HOST,
+		port: process.env.SMTP_PORT,
+		service: process.env.SMTP_SERVICE,
 		auth: {
-			user: process.env.SMPT_MAIL,
-			pass: process.env.SMPT_APP_PASS,
+			user: process.env.SMTP_MAIL,
+			pass: process.env.SMTP_APP_PASS,
 		},
 	});
 
