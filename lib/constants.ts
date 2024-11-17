580062f721679288ebb6335b4123526a3ac6ade8
Darek Mazur <dev@darekmazur.it>
2024-11-17 18:49:07 +0100

    Create entropy method to check password strength

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,33 @@ export const emailVerification = (email: string) => {
 
 	return emailRegex.test(email)
 }
+
+export const entropy = (string: string) => {
+	const uppercase = /[^A-Z]/;
+	const lowercase = /[^a-z]/;
+	const numbers = /[^0-9]/;
+	const specials = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/;
+
+	const length = string.length
+	let r = 0
+
+	if (uppercase.test(string)) {
+		r += 26
+	}
+
+	if (lowercase.test(string)) {
+		r += 26
+	}
+
+	if (numbers.test(string)) {
+		r += 10
+	}
+
+	if (specials.test(string)) {
+		r += 33
+	}
+
+	return length * Math.log2(r)
+}
+
+export const acceptedEntropy = 70
